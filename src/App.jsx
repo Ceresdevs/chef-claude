b2cbde4d3dd328d5b69a7f997e3038110dbde9a0
Fortunato Galapon <ceresceresdev@gmail.com>
2026-09-04 20:52:14 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+
+function Page() {
+  return (
+    <>
+     <Header />
+     <Main />
+    </>
+  )
+}
+
+function App() {
+  return (
+   <Page />
+  )
+}
+
+
+
+export default App
---
+++ src/components/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+    return (
+        <main>
+            <form className="ingredient-form">
+                <input type="text" aria-label="Add ingredient" placeholder="e.g. Chicken" />
+                <button>Add ingredient</button>
+            </form>
+        </main> 
+    )
+}
